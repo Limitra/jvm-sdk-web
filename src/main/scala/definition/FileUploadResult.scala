46b59f7fc5bd66686c93022ef49c0a89949e6378
Mustafa Nevfel <me@mnevfel.com>
2019-07-13 06:40:38 +0300

    File upload json result type generated.

---
+++ src/main/scala/definition/FileUploadResult.scala
@@ -0,0 +1,13 @@
+package com.limitra.sdk.web.definition
+
+import play.api.libs.json.Json
+
+case class FileUploadResult(var Path: String = "",
+                            var Name: String = "",
+                            var Type: Option[String] = None,
+                            var Size: Long = 0)
+
+object FileUploadResult {
+  implicit val writes = Json.writes[FileUploadResult]
+  implicit val reads = Json.reads[FileUploadResult]
+}
