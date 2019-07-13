accd3710bfb4538490c0b1dfc96219d10e5a6d2b
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-13 06:41:24 +0300

    File storage schedule task actor generated.

---
+++ src/main/scala/task/FileStorageTask.scala
@@ -0,0 +1,40 @@
+package com.limitra.sdk.web.task
+
+import java.io.File
+
+import akka.actor.ActorSystem
+import com.limitra.sdk.core.Config
+import javax.inject.Inject
+import org.joda.time.DateTime
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class FileStorageTask @Inject()(actorSystem: ActorSystem)(implicit executionContext: ExecutionContext) {
+  private val _config = Config("File")
+
+  actorSystem.scheduler.schedule(initialDelay = 10.seconds, interval = 10.seconds) {
+    val source = this._config.OptionString("Path")
+    if (source.isDefined) {
+      val temp = (source.get + "/temp").replace("//", "/")
+      val folder = new File(temp)
+      if (folder.exists && folder.isDirectory) {
+        folder.listFiles.toList.foreach(file => {
+          if (file.isDirectory) {
+            file.listFiles.toList.foreach(sub => {
+              this._deleteFile(sub)
+            })
+          } else {
+            this._deleteFile(file)
+          }
+        })
+      }
+    }
+  }
+
+  private def _deleteFile(file: File) {
+    if (file.lastModified < DateTime.now.minusSeconds(30).getMillis) {
+      file.delete()
+    }
+  }
+}
