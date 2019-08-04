e2edede6175bffaa9099b6787e4c4466d3950626
Mustafa Nevfel <me@mnevfel.com>
2019-08-04 17:12:42 +0300

    Response provider initialized from sdk web initializer.

--- src/main/scala/WebInitializer.scala
+++ src/main/scala/WebInitializer.scala
@@ -1,6 +1,9 @@
 package com.limitra.sdk
 
+import com.limitra.sdk.core.{definition => df}
+import com.limitra.sdk.core.extension._
 import com.limitra.sdk.web.extension._
+import com.limitra.sdk.web.provider._
 import play.api.data.Form
 import play.api.libs.json._
 import play.api.mvc.Request
@@ -8,7 +11,15 @@ import play.api.mvc.Request
 package object web {
   implicit def RequestExt[A](value: Request[A]) = new RequestExtender[A](value)
 
-  implicit def FormExt[A](value: Form[A]) = new FormExtender[A](value)
+  implicit def FormExt[A](value: Form[A])(implicit request: Request[A]) = new FormExtender[A](value)(request)
 
   implicit def SingleClassExt[A](value: A)(implicit wr: Writes[A]) = new SingleClassExtender[A](value)
+
+  def DateTime(implicit request: Request[_]) = new df.DateTime(request.TimeZone)
+
+  def Response(implicit request: Request[_]): ResponseProvider = new ResponseProvider(request.Language)
+
+  implicit def LongExt(value: Long)(implicit request: Request[_]) = new LongExtender(request.Language, value)
+
+  implicit def BigDecimalExt(value: BigDecimal)(implicit request: Request[_]) = new BigDecimalExtender(request.Language, value)
 }
