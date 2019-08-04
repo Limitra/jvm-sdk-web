24d919983a8e695bcb721cea092eb327362018e3
mnevfel <mnevfel@limitra.com>
2019-08-04 16:46:42 +0300

    Web provider generated for read localized http responses.

---
+++ src/main/scala/provider/ResponseProvider.scala
@@ -0,0 +1,30 @@
+package com.limitra.sdk.web.provider
+
+import java.io.File
+
+import com.limitra.sdk.core.Config
+import play.api.libs.json.{JsValue, Json}
+
+import scala.io.Source
+
+/**
+ * It is used to make customized and localized http response messages.
+ */
+sealed class ResponseProvider(lang: Option[String]) {
+  private val _app = Config("Application")
+  private val _path = _app.OptionString("Root")
+  private val _response = _app.Get("Text").OptionString("Response")
+  private var _lang = lang.getOrElse("")
+  if (!new File(_path.get + "/" + _response + "/" + _lang).exists()) {
+    _lang = _app.OptionString("DefaultLang").getOrElse("en-US")
+  }
+  private val _text: JsValue = if(_response.isDefined && _path.isDefined) Json.parse(Source.fromFile(_path.get + "/" + _response + "/" + _lang).getLines.mkString) else null
+
+  def Read(key: String): String = {
+    var text: Option[String] = None
+    if (lang.isDefined) {
+      text = if(_text != null) (_text \ key).asOpt[String] else None
+    }
+    text.getOrElse("-")
+  }
+}
