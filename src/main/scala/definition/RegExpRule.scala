16ee9927aa55267cd6ee17abe545132ff8d78cf4
mnevfel <mnevfel@limitra.com>
2019-08-11 12:16:18 +0300

    RegExp object added to source.

---
+++ src/main/scala/definition/RegExpRule.scala
@@ -0,0 +1,6 @@
+package com.limitra.sdk.web
+
+object RegExpRule {
+  val Number: String = """^[0-9]{1,7}$"""
+  val Email: String = """^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+}
