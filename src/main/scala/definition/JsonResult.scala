df3a20acfc5bcfde8a553bfdbb5bff1afc24284d
mnevfel <mnevfel@limitra.com>
2019-08-11 17:27:58 +0300

    Response token field added to json result class.

--- src/main/scala/definition/JsonResult.scala
+++ src/main/scala/definition/JsonResult.scala
@@ -3,6 +3,7 @@ package com.limitra.sdk.web.definition
 import play.api.libs.json.Json
 
 case class JsonResult(var ResponseText: String = "",
+                      var ResponseToken: String = "",
                       var Notification: JsonResultNotify = null
                      )
 
