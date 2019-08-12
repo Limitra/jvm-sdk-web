3f39ab2d3b4c3c93228aa8148dfd6de5c2eb4c99
Mustafa Nevfel <me@mnevfel.com>
2019-08-12 21:39:26 +0300

    Dynamic composition parameter type converted to jwt from request.

--- src/main/scala/composition/Composition.scala
+++ src/main/scala/composition/Composition.scala
@@ -2,6 +2,7 @@ package com.limitra.sdk.web
 
 import com.limitra.sdk.database.mysql.DbSource
 import com.limitra.sdk.web.composition._
+import com.limitra.sdk.web.definition.JsonWebToken
 import play.api.mvc.{ActionBuilder, ActionBuilderImpl, AnyContent, BodyParser, Request}
 import slick.lifted.Rep
 
@@ -9,7 +10,7 @@ import scala.concurrent.ExecutionContext
 
 abstract class AbstractComposition(parser: BodyParser[AnyContent])(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   def Db: DbSource
-  def IsAuthenticated(request: Request[_]): Rep[Boolean]
+  def IsAuthenticated(jwt: JsonWebToken): Rep[Boolean]
 
   def Authenticated: ActionBuilder[Request, AnyContent] = {
     return new Authentication(parser, this.Db, this.IsAuthenticated)(ec)
