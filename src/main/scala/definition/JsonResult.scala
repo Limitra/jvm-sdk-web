556c30c7481f1a8de849f830b347f7ad330d42cb
Mustafa Nevfel <me@mnevfel.com>
2019-08-12 21:10:47 +0300

    Text parameter set method generated for localized assign.

--- src/main/scala/definition/JsonResult.scala
+++ src/main/scala/definition/JsonResult.scala
@@ -1,11 +1,17 @@
 package com.limitra.sdk.web.definition
 
+import com.limitra.sdk.web.Response
 import play.api.libs.json.Json
+import play.api.mvc.Request
 
 case class JsonResult(var Status: Option[Int] = None,
                       var Text: Option[String] = None,
                       var Notification: JsonResultNotify = null
-                     )
+                     ) {
+  def SetText(message: String)(implicit request: Request[_]): Unit = {
+    this.Text = Response.ReadOption(message);
+  }
+}
 
 object JsonResult {
   implicit val writes = Json.writes[JsonResult]
