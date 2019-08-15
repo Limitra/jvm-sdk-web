3a9be14b25e28293deaca01c4150b7c356f2f5cb
Mustafa Nevfel <me@mnevfel.com>
2019-08-15 19:05:54 +0300

    JsonResult none setter logic applied.

--- src/main/scala/definition/JsonResult.scala
+++ src/main/scala/definition/JsonResult.scala
@@ -13,21 +13,24 @@ case class JsonResult(var Status: Option[Int] = None,
   }
 
   def SetTitle(title: String)(implicit request: Request[_]): Unit = {
-    if (this.Notification.isDefined) {
-      this.Notification.get.Title = Response.Read(title);
+    if (!this.Notification.isDefined) {
+      this.Notification = Some(new JsonResultNotify())
     }
+    this.Notification.get.Title = Response.Read(title);
   }
 
   def SetStatus(status: String)(implicit request: Request[_]): Unit = {
-    if (this.Notification.isDefined) {
-      this.Notification.get.Status = status;
+    if (!this.Notification.isDefined) {
+      this.Notification = Some(new JsonResultNotify())
     }
+    this.Notification.get.Status = status;
   }
 
   def SetMessage(message: String)(implicit request: Request[_]): Unit = {
-    if (this.Notification.isDefined) {
-      this.Notification.get.Message = Response.Read(message);
+    if (!this.Notification.isDefined) {
+      this.Notification = Some(new JsonResultNotify())
     }
+    this.Notification.get.Message = Response.Read(message);
   }
 }
 
