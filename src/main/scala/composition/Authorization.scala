18da892f5be2c568d1989c4abac977e44198470b
mnevfel <mnevfel@limitra.com>
2019-08-15 19:05:22 +0300

    Http jwt authenticatino upgraded and authorize composition generated.

---
+++ src/main/scala/composition/Authorization.scala
@@ -0,0 +1,46 @@
+package com.limitra.sdk.web.composition
+
+import com.limitra.sdk.database.mysql.DbSource
+import com.limitra.sdk.web._
+import com.limitra.sdk.web.definition._
+import play.api.mvc._
+import slick.lifted.{Query, Rep}
+
+import scala.concurrent._
+
+sealed class Authorization(parser: BodyParser[AnyContent], db: DbSource, query: (JsonWebToken) => Query[_, String, Seq])
+                          (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
+  import db._
+  var jresult = new JsonResult
+
+  override def invokeBlock[A](request: Request[A],
+                              block: (Request[A]) => Future[Result]) = {
+    val jwt = request.ToJwt
+
+    val unauthorized = () => {
+      jresult.Status = Some(401)
+      jresult.SetText("Unauthorized")(request)
+      jresult.SetTitle("Title")(request)
+      jresult.SetMessage("Unauthorized")(request)
+      Future.successful(Results.Unauthorized(jresult.ToJson))
+    }
+
+    if (jwt.isDefined && jwt.get.IsValid) {
+      var valid: Boolean = false
+      val routes: Seq[String] = this.query(jwt.get).Execute
+      routes.foreach(route => {
+        var uri = ""
+        route.split('/').foreach(partial => {
+          uri = uri + (if (partial.contains(":")) "/*." else "/" + partial)
+        })
+        uri = uri.replace("//", "/")
+        if (request.path.matches(uri)) {
+          valid = true
+        }
+      })
+      if (valid) {
+        block(request)
+      } else { unauthorized() }
+    } else { unauthorized() }
+  }
+}
--- src/main/scala/composition/Composition.scala
+++ src/main/scala/composition/Composition.scala
@@ -4,15 +4,20 @@ import com.limitra.sdk.database.mysql.DbSource
 import com.limitra.sdk.web.composition._
 import com.limitra.sdk.web.definition.JsonWebToken
 import play.api.mvc.{ActionBuilder, ActionBuilderImpl, AnyContent, BodyParser, Request}
-import slick.lifted.Rep
+import slick.lifted.{Query, Rep}
 
 import scala.concurrent.ExecutionContext
 
 abstract class AbstractComposition(parser: BodyParser[AnyContent])(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   def Db: DbSource
   def IsAuthenticated(jwt: JsonWebToken): Rep[Boolean]
+  def IsAuthorized(jwt: JsonWebToken): Query[_, String, Seq]
 
   def Authenticated: ActionBuilder[Request, AnyContent] = {
     return new Authentication(parser, this.Db, this.IsAuthenticated)(ec)
+  }
+
+  def Authorized: ActionBuilder[Request, AnyContent] = {
+    return (new Authentication(parser, this.Db, this.IsAuthenticated)(ec) andThen new Authorization(parser, this.Db, this.IsAuthorized)(ec))
   }
 }
