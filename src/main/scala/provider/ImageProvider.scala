8bb30fdc96c7ad4877648312bfee33d314166d15
Mustafa Nevfel <me@mnevfel.com>
2019-10-07 14:50:50 +0300

    Image compression upgraded and write text function generated.

--- src/main/scala/provider/ImageProvider.scala
+++ src/main/scala/provider/ImageProvider.scala
@@ -1,11 +1,14 @@
 package com.limitra.sdk.web.provider
 
-import java.io.File
+import java.io.{File, FileOutputStream}
 
 import com.limitra.sdk.core.Config
 import com.limitra.sdk.web
 import com.sksamuel.scrimage.nio.{JpegWriter, PngWriter}
 import com.sksamuel.scrimage.{Color, Image}
+import javax.imageio.ImageIO
+import java.awt.Font
+import java.awt.image.BufferedImage
 
 /**
  * It is used to make customized and image operations.
@@ -13,7 +16,7 @@ import com.sksamuel.scrimage.{Color, Image}
 sealed class ImageProvider {
   private val _config = Config("Application").Get("File")
 
-  def Resize(width: Int, height: Int, path: String, ride: Boolean = false): Option[String] = {
+  def Resize(width: Int, height: Int, path: String, ride: Boolean = false, compressForce: Boolean = false): Option[String] = {
     web.File._init()
     val source = this._config.OptionString("Path")
     if (source.isDefined) {
@@ -26,17 +29,48 @@ sealed class ImageProvider {
           newPath = path.replace("." + partials.last, "") + "_" + width.toString + "x" + height.toString + "." + partials.last
         }
         map = (source.get + "/" + newPath).replace("//", "/")
-        val rgb = if (newPath.toLowerCase().endsWith("png")) Color.Transparent else Color.Black
-        if (newPath.toLowerCase().endsWith("png")) {
+        val rgb = if (newPath.toLowerCase().endsWith("png") && !compressForce) Color.Transparent else Color.Black
+        if (newPath.toLowerCase().endsWith("png") && !compressForce) {
           implicit val writer = PngWriter.MaxCompression
+          Image.fromFile(file).fit(width, height, rgb).output(new File(map))
         } else {
           implicit val writer = JpegWriter().withCompression(50)
+          Image.fromFile(file).fit(width, height, rgb).output(new File(map))
         }
-        Image.fromFile(file).fit(width, height, rgb).output(new File(map))
         if (new File(map).exists()) {
           Some(newPath)
         } else { None }
       } else { None }
     } else { None }
+  }
+
+  def WriteText(path: String, text: String, compressForce: Boolean = false): Boolean = {
+    web.File._init()
+    val source = this._config.OptionString("Path")
+    if (source.isDefined) {
+      val map = (source.get + "/" + path).replace("//", "/")
+      val file = new java.io.File(map)
+      if (file.exists()) {
+        val font = new Font("Arial Black", Font.BOLD, 20)
+        val bufferedImage = Image.fromFile(file).toNewBufferedImage()
+        val graphics = bufferedImage.getGraphics
+        graphics.setColor(new java.awt.Color(255, 255, 255, 150))
+        graphics.setFont(font)
+
+        val metrics = graphics.getFontMetrics(font)
+        val x = (bufferedImage.getWidth - metrics.stringWidth(text)) / 2
+        val y = ((bufferedImage.getHeight() - metrics.getHeight) / 2) + metrics.getAscent
+
+        graphics.drawString(text, x, y)
+        if (path.toLowerCase().endsWith("png") && !compressForce) {
+          implicit val writer = PngWriter.MaxCompression
+          writer.write(bufferedImage, new FileOutputStream(file))
+        } else {
+          implicit val writer = JpegWriter().withCompression(50)
+          writer.write(bufferedImage, new FileOutputStream(file))
+        }
+        true
+      } else { false }
+    } else { false }
   }
 }
