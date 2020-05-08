4969514d1d3a3ba6f3bde65d44a4ee17b6da85a1
Mustafa Nevfel <mnevfel@limitra.com>
2020-05-08 14:32:46 +0300

    UserID property added to device info type.

--- src/main/scala/definition/DeviceInfo.scala
+++ src/main/scala/definition/DeviceInfo.scala
@@ -1,6 +1,7 @@
 package com.limitra.sdk.web.definition
 
-case class DeviceInfo(var UserAgent: Option[String] = None,
+case class DeviceInfo(var UserID: Option[Long] = None,
+                      var UserAgent: Option[String] = None,
                       var OS: Option[String] = None,
                       var OSVersion: Option[String] = None,
                       var Browser: Option[String] = None,
