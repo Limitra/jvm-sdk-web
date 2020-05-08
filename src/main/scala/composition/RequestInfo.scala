c38f5029f4cc2c953219791f2ccb61f42d9d9af8
Mustafa Nevfel <mnevfel@limitra.com>
2020-05-08 16:03:54 +0300

    DeviceInfo renamed to RequestInfo.

--- src/main/scala/composition/RequestInfo.scala
+++ src/main/scala/composition/RequestInfo.scala
@@ -6,13 +6,13 @@ import play.api.mvc._
 
 import scala.concurrent._
 
-sealed class DeviceInfo(parser: BodyParser[AnyContent], info: (definition.DeviceInfo) => Unit)
+sealed class RequestInfo(parser: BodyParser[AnyContent], info: (definition.RequestInfo) => Unit)
                    (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   override def invokeBlock[A](request: Request[A],
                               block: (Request[A]) => Future[Result]) = {
     val jwt = request.ToJwt
 
-    val device = new definition.DeviceInfo
+    val device = new definition.RequestInfo
     device.UserAgent = request.headers.get("UserAgent")
     if (!device.UserAgent.isDefined) {
       device.UserAgent = request.headers.get("User-Agent")
@@ -23,6 +23,11 @@ sealed class DeviceInfo(parser: BodyParser[AnyContent], info: (definition.Device
     device.DeviceType = request.headers.get("DeviceType")
     device.OS = request.headers.get("OS")
     device.OSVersion = request.headers.get("OSVersion")
+
+    device.RemoteAddress = request.RemoteAddress
+    device.Path = request.path
+    device.Header = Some(request.headers.toString)
+    device.Body = if (request.hasBody) Some(request.body.toString) else None
 
     if (jwt.isDefined) {
       device.UserID = Some(jwt.get.ID)
