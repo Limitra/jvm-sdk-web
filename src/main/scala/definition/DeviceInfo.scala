00ae8578636b8f04e3c65dc8cc8d037f07bf99c8
Mustafa Nevfel <me@mnevfel.com>
2020-05-08 13:16:44 +0300

    Device information type class generated.

---
+++ src/main/scala/definition/DeviceInfo.scala
@@ -0,0 +1,10 @@
+package com.limitra.sdk.web.definition
+
+case class DeviceInfo(var UserAgent: Option[String] = None,
+                      var OS: Option[String] = None,
+                      var OSVersion: Option[String] = None,
+                      var Browser: Option[String] = None,
+                      var BrowserVersion: Option[String] = None,
+                      var Device: Option[String] = None,
+                      var DeviceType: Option[String] = None)
+
