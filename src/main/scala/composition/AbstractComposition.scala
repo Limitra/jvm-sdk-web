85503f5c2039a4a4217882760fb5cdcbd3ce1490
Mustafa Nevfel <mnevfel@limitra.com>
2020-05-08 16:37:27 +0300

    RequestInfo and Security compositions merged on Security composition.

--- src/main/scala/composition/AbstractComposition.scala
+++ src/main/scala/composition/AbstractComposition.scala
@@ -10,20 +10,19 @@ import scala.concurrent.ExecutionContext
 
 abstract class AbstractComposition(parser: BodyParser[AnyContent])(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   def Db: DbSource
-  def GetInfo(info: definition.RequestInfo): Unit = () => {}
-  def IsSecured(token: Option[String]): Boolean = { return true }
+  def IsSecured(token: Option[String], info: definition.RequestInfo): Boolean = { return true }
   def IsAuthenticated(jwt: JsonWebToken): Rep[Boolean]
   def AuthorizedUrls(jwt: JsonWebToken): Query[_, String, Seq]
 
   def Secured: ActionBuilder[Request, AnyContent] = {
-    return new RequestInfo(parser, this.GetInfo)(ec) andThen new Security(parser, this.IsSecured)(ec)
+    return new Security(parser, this.IsSecured)(ec)
   }
 
   def Authenticated: ActionBuilder[Request, AnyContent] = {
-    return new RequestInfo(parser, this.GetInfo)(ec) andThen new Authentication(parser, this.Db, this.IsAuthenticated)(ec) andThen new Security(parser, this.IsSecured)(ec)
+    return new Authentication(parser, this.Db, this.IsAuthenticated)(ec) andThen new Security(parser, this.IsSecured)(ec)
   }
 
   def Authorized: ActionBuilder[Request, AnyContent] = {
-    return new RequestInfo(parser, this.GetInfo)(ec) andThen new Authentication(parser, this.Db, this.IsAuthenticated)(ec) andThen new Authorization(parser, this.Db, this.AuthorizedUrls)(ec) andThen new Security(parser, this.IsSecured)(ec)
+    return new Authentication(parser, this.Db, this.IsAuthenticated)(ec) andThen new Authorization(parser, this.Db, this.AuthorizedUrls)(ec) andThen new Security(parser, this.IsSecured)(ec)
   }
 }
--- src/main/scala/composition/Security.scala
+++ src/main/scala/composition/Security.scala
@@ -1,21 +1,46 @@
 package com.limitra.sdk.web.composition
 
 import com.limitra.sdk.database.mysql.DbSource
-import com.limitra.sdk.web._
+import com.limitra.sdk.web.{definition, _}
 import com.limitra.sdk.web.definition._
 import play.api.mvc._
 import slick.lifted.Rep
 
 import scala.concurrent._
 
-sealed class Security(parser: BodyParser[AnyContent], header: (Option[String]) => Boolean)
+sealed class Security(parser: BodyParser[AnyContent], check: (Option[String], definition.RequestInfo) => Boolean)
                      (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   var jresult = new JsonResult()
 
   override def invokeBlock[A](request: Request[A],
                               block: (Request[A]) => Future[Result]) = {
     val token = request.headers.get("SecurityToken")
-    val secured = header(token)
+    val jwt = request.ToJwt
+
+    val reqInfo = new definition.RequestInfo
+    reqInfo.UserAgent = request.headers.get("UserAgent")
+    if (!reqInfo.UserAgent.isDefined) {
+      reqInfo.UserAgent = request.headers.get("User-Agent")
+    }
+    reqInfo.Browser = request.headers.get("Browser")
+    reqInfo.BrowserVersion = request.headers.get("BrowserVersion")
+    reqInfo.Device = request.headers.get("Device")
+    reqInfo.DeviceType = request.headers.get("DeviceType")
+    reqInfo.OS = request.headers.get("OS")
+    reqInfo.OSVersion = request.headers.get("OSVersion")
+
+
+    reqInfo.Millis = DateTime(request).now.getMillis
+    reqInfo.RemoteAddress = request.RemoteAddress
+    reqInfo.Path = request.path
+    reqInfo.Header = request.headers.headers.mkString(", ")
+    reqInfo.Body = if (request.hasBody) Some(request.body.toString) else None
+
+    if (jwt.isDefined) {
+      reqInfo.UserID = Some(jwt.get.ID)
+    }
+
+    val secured = check(token, reqInfo)
 
     val forbidden = () => {
       jresult.Status = Some(403)
