708801cbf391411cf668dba9bf879ff87941bdf0
Mustafa Nevfel <me@mnevfel.com>
2020-05-08 16:09:41 +0300

    Request info renamed with variable names.

--- src/main/scala/composition/RequestInfo.scala
+++ src/main/scala/composition/RequestInfo.scala
@@ -12,28 +12,30 @@ sealed class RequestInfo(parser: BodyParser[AnyContent], info: (definition.Reque
                               block: (Request[A]) => Future[Result]) = {
     val jwt = request.ToJwt
 
-    val device = new definition.RequestInfo
-    device.UserAgent = request.headers.get("UserAgent")
-    if (!device.UserAgent.isDefined) {
-      device.UserAgent = request.headers.get("User-Agent")
+    val reqInfo = new definition.RequestInfo
+    reqInfo.UserAgent = request.headers.get("UserAgent")
+    if (!reqInfo.UserAgent.isDefined) {
+      reqInfo.UserAgent = request.headers.get("User-Agent")
     }
-    device.Browser = request.headers.get("Browser")
-    device.BrowserVersion = request.headers.get("BrowserVersion")
-    device.Device = request.headers.get("Device")
-    device.DeviceType = request.headers.get("DeviceType")
-    device.OS = request.headers.get("OS")
-    device.OSVersion = request.headers.get("OSVersion")
+    reqInfo.Browser = request.headers.get("Browser")
+    reqInfo.BrowserVersion = request.headers.get("BrowserVersion")
+    reqInfo.Device = request.headers.get("Device")
+    reqInfo.DeviceType = request.headers.get("DeviceType")
+    reqInfo.OS = request.headers.get("OS")
+    reqInfo.OSVersion = request.headers.get("OSVersion")
 
-    device.RemoteAddress = request.RemoteAddress
-    device.Path = request.path
-    device.Header = Some(request.headers.toString)
-    device.Body = if (request.hasBody) Some(request.body.toString) else None
+
+    reqInfo.Millis = DateTime(request).now.getMillis
+    reqInfo.RemoteAddress = request.RemoteAddress
+    reqInfo.Path = request.path
+    reqInfo.Header = Some(request.headers.toString)
+    reqInfo.Body = if (request.hasBody) Some(request.body.toString) else None
 
     if (jwt.isDefined) {
-      device.UserID = Some(jwt.get.ID)
+      reqInfo.UserID = Some(jwt.get.ID)
     }
 
-    info(device)
+    info(reqInfo)
 
     block(request)
   }
