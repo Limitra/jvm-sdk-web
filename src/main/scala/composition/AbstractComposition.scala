c28770495ad0382a3692f5a71b325e0a17d97b48
Mustafa Nevfel <mnevfel@limitra.com>
2020-05-09 06:22:22 +0300

    Information composition applied with request info.

--- src/main/scala/composition/AbstractComposition.scala
+++ src/main/scala/composition/AbstractComposition.scala
@@ -2,7 +2,7 @@ package com.limitra.sdk.web.composition
 
 import com.limitra.sdk.web.{definition=>definition}
 import com.limitra.sdk.database.mysql.DbSource
-import com.limitra.sdk.web.definition.{RequestInfo, JsonWebToken}
+import com.limitra.sdk.web.definition.{JsonWebToken}
 import play.api.mvc.{ActionBuilder, ActionBuilderImpl, AnyContent, BodyParser, Request}
 import slick.lifted.{Query, Rep}
 
@@ -10,9 +10,14 @@ import scala.concurrent.ExecutionContext
 
 abstract class AbstractComposition(parser: BodyParser[AnyContent])(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   def Db: DbSource
+  def Informed(info: definition.RequestInfo): Unit = () => {}
   def IsSecured(token: Option[String], info: definition.RequestInfo): Boolean = { return true }
   def IsAuthenticated(jwt: JsonWebToken): Rep[Boolean]
   def AuthorizedUrls(jwt: JsonWebToken): Query[_, String, Seq]
+
+  def Informed: ActionBuilder[Request, AnyContent] = {
+    return new Information(parser, this.Informed)(ec)
+  }
 
   def Secured: ActionBuilder[Request, AnyContent] = {
     return new Security(parser, this.IsSecured)(ec)
