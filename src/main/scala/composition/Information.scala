1658ffacf6dd67196cb84c04b6d4446deddf3924
Mustafa Nevfel <me@mnevfel.com>
2020-05-09 06:22:22 +0300

    Information composition applied with request info.

---
+++ src/main/scala/composition/Information.scala
@@ -0,0 +1,41 @@
+package com.limitra.sdk.web.composition
+
+import com.limitra.sdk.web.{definition, _}
+import play.api.mvc._
+
+import scala.concurrent._
+
+sealed class Information(parser: BodyParser[AnyContent], info: (definition.RequestInfo) => Unit)
+                        (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
+  override def invokeBlock[A](request: Request[A],
+                              block: (Request[A]) => Future[Result]) = {
+    val jwt = request.ToJwt
+
+    val reqInfo = new definition.RequestInfo
+    reqInfo.UserAgent = request.headers.get("UserAgent")
+    if (!reqInfo.UserAgent.isDefined) {
+      reqInfo.UserAgent = request.headers.get("User-Agent")
+    }
+    reqInfo.Browser = request.headers.get("Browser")
+    reqInfo.BrowserVersion = request.headers.get("BrowserVersion")
+    reqInfo.Device = request.headers.get("Device")
+    reqInfo.DeviceType = request.headers.get("DeviceType")
+    reqInfo.OS = request.headers.get("OS")
+    reqInfo.OSVersion = request.headers.get("OSVersion")
+
+
+    reqInfo.Millis = DateTime(request).now.getMillis
+    reqInfo.RemoteAddress = request.RemoteAddress
+    reqInfo.Path = request.path
+    reqInfo.Header = request.headers.headers.mkString(", ")
+    reqInfo.Body = if (request.hasBody) Some(request.body.toString) else None
+
+    if (jwt.isDefined) {
+      reqInfo.UserID = Some(jwt.get.ID)
+    }
+
+    info(reqInfo)
+
+    block(request)
+  }
+}
